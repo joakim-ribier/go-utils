fbc8ea8356e586c8848d9d4eb384960984a1c7ac
Joakim Ribier <joakim.ribier@gmail.com>
2024-02-11 20:53:10 +0100

    test: update coverage on 'jsonsutil' package

--- pkg/jsonsutil/jsonsutil_test.go
+++ pkg/jsonsutil/jsonsutil_test.go
@@ -10,6 +10,37 @@ type UserTestMock struct {
 	Name string
 }
 
+// TestUnmarshalWithBadJson calls jsonsutil.Unmarshal,
+// checking for a valid return value.
+func TestLoadFWithBadFilename(t *testing.T) {
+	r, err := LoadF[UserTestMock]("file-does-not-exist.json")
+	if err == nil {
+		t.Fatalf(`result: {%v} but expected error`, r)
+	}
+}
+
+// TestUnmarshalWithBadJson calls jsonsutil.Unmarshal,
+// checking for a valid return value.
+func TestUnmarshalWithBadJson(t *testing.T) {
+	r, err := Unmarshal[UserTestMock]([]byte("{broken json data...}"))
+	if err == nil {
+		t.Fatalf(`result: {%v} but expected error`, r)
+	}
+}
+
+// TestUnmarshal calls jsonsutil.Unmarshal,
+// checking for a valid return value.
+func TestUnmarshal(t *testing.T) {
+	user := UserTestMock{Age: 18, Name: "Bob"}
+	if r, err := Unmarshal[UserTestMock]([]byte("{\"Age\": 18, \"Name\": \"Bob\"}")); err == nil {
+		if r != user {
+			t.Fatalf(`result: {%v} but expected: {%v}`, r, user)
+		}
+	} else {
+		t.Fatal(err)
+	}
+}
+
 // TestWriteTAndLoadF calls jsonsutil.WriteT and jsonsutil.LoadF,
 // checking for a valid return value.
 func TestWriteTAndLoadF(t *testing.T) {
@@ -22,19 +53,6 @@ func TestWriteTAndLoadF(t *testing.T) {
 			t.Fatalf(`result: {%v} but expected: {%v}`, r, user)
 		}
 		os.Remove("users.json")
-	} else {
-		t.Fatal(err)
-	}
-}
-
-// TestUnmarshal calls jsonsutil.Unmarshal,
-// checking for a valid return value.
-func TestUnmarshal(t *testing.T) {
-	user := UserTestMock{Age: 18, Name: "Bob"}
-	if r, err := Unmarshal[UserTestMock]([]byte("{\"Age\": 18, \"Name\": \"Bob\"}")); err == nil {
-		if r != user {
-			t.Fatalf(`result: {%v} but expected: {%v}`, r, user)
-		}
 	} else {
 		t.Fatal(err)
 	}
