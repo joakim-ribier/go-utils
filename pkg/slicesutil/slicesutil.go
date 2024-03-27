5119990651e8ed1f31c1dd33fa67fa92b9b01ea1
Joakim Ribier <joakim.ribier@gmail.com>
2024-03-27 19:56:42 +0100

    feat: implement 'Distinct; Equal; Must; ContainAll; FatTransformT' functions

--- pkg/slicesutil/slicesutil.go
+++ pkg/slicesutil/slicesutil.go
@@ -12,14 +12,24 @@ type SliceS []string
 // #### SliceS type functions ####
 // ##
 
+// NewSliceS builds {SliceS} type from {in}.
+func NewSliceS(in []string) SliceS {
+	return append(SliceS{}, in...)
+}
+
 // Append appends the slice {to} into slice without duplicated values (case-insensitive).
-func (s SliceS) Append(to []string) []string {
+func (s SliceS) Append(to []string) SliceS {
 	return Append(s, to)
 }
 
 // Clone creates a new copy of slice.
-func (s SliceS) Clone() []string {
-	return CloneT(s)
+func (s SliceS) Clone() SliceS {
+	return slices.Clone(s)
+}
+
+// Distinct removes duplicated values in the slice.
+func (s SliceS) Distinct() SliceS {
+	return Distinct(s)
 }
 
 // Exist returns true if {v} value exists in slice (case-insensitive).
@@ -68,6 +78,26 @@ func Append(from, to []string) []string {
 	return AppendT(from, to, func(f, t string) bool { return strings.EqualFold(f, t) })
 }
 
+// ContainAll returns {true} if every value of the slice {s1} is in the slice {s2} (case-insensitive).
+func ContainAll(s1, s2 []string) bool {
+	for _, v := range s1 {
+		if !Exist(s2, v) {
+			return false
+		}
+	}
+	return true
+}
+
+// Distinct removes duplicated values in the slice {in}.
+func Distinct(in []string) []string {
+	return Append([]string{}, in)
+}
+
+// Equal returns {true} if the two slices contain exactly the same values (case-insensitive).
+func Equal(s1, s2 []string) bool {
+	return len(s1) == len(s2) && ContainAll(s1, s2) && ContainAll(s2, s1)
+}
+
 // Exist returns {true} if {v} value exists in the slice {s} (case-insensitive).
 func Exist(s []string, v string) bool {
 	return ExistT(s, func(el string) bool { return strings.EqualFold(el, v) })
@@ -76,6 +106,11 @@ func Exist(s []string, v string) bool {
 // FilterByNonEmpty removes all empty values in the slice {in}.
 func FilterByNonEmpty(in []string) []string {
 	return FilterT(in, func(s string) bool { return strings.TrimSpace(s) != "" })
+}
+
+// Must returns {true} if the two slices contain exactly in the same order the same values (case-insensitive).
+func Must(s1, s2 []string) bool {
+	return slices.EqualFunc(s1, s2, strings.EqualFold)
 }
 
 // ToMap transforms the slice {in} to a map key/value.
@@ -93,7 +128,7 @@ func ToMap(in []string) map[string]string {
 //
 // returns empty if no occurrence found.
 func FindLastOccurrenceIn(from, to []string) string {
-	s := CloneT(from)
+	s := slices.Clone(from)
 	slices.Reverse(s)
 
 	for _, v := range s {
@@ -140,18 +175,13 @@ func AddOrReplaceT[T any](in []T, t T, is func(T) bool) []T {
 
 // Append appends the slice {to} into the slice {from} without duplicated values using the provided {exist} function.
 func AppendT[T any](from, to []T, exist func(T, T) bool) []T {
-	s := CloneT(from)
+	s := slices.Clone(from)
 	for _, t := range to {
 		if !ExistT(s, func(el T) bool { return exist(el, t) }) {
 			s = append(s, t)
 		}
 	}
 	return s
-}
-
-// CloneT creates new copy of the slice {s}.
-func CloneT[T any](s []T) []T {
-	return append([]T{}, s...)
 }
 
 // ExistT returns bool using the provided equal function.
@@ -179,9 +209,20 @@ func FindT[T any](in []T, is func(T) bool) *T {
 	}
 }
 
+// FlatTransformT transforms and flattens the slice {from} []F to the slice []T using the provided {transform} function.
+func FlatTransformT[F any, T any](from []F, transform func(F) ([]T, error)) []T {
+	var to []T
+	for _, el := range from {
+		if v, err := transform(el); err == nil && v != nil {
+			to = append(to, v...)
+		}
+	}
+	return to
+}
+
 // SortT clones and sorts the slice {s} using the provided {less} function.
 func SortT[T any](s []T, less func(T, T) bool) []T {
-	var out = CloneT(s)
+	var out = slices.Clone(s)
 	sort.SliceStable(out, func(i, j int) bool {
 		return less(s[i], s[j])
 	})
