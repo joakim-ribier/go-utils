d0325b46dfa034bdf0bd25ff2f62a39c6b13c927
Joakim Ribier <joakim.ribier@gmail.com>
2024-07-24 22:09:49 +0200

    Task: use 'joakimribier/gmocky-v2' container to execute tests (#4)

--- go.mod
+++ go.mod
@@ -1,16 +1,27 @@
 module github.com/joakim-ribier/go-utils
 
-go 1.21.1
+go 1.22.5
+
+require github.com/ory/dockertest v3.3.5+incompatible
 
 require (
-	github.com/go-logr/logr v1.4.1
-	github.com/go-logr/zapr v1.3.0
-	github.com/robfig/cron v1.2.0
-	go.uber.org/zap v1.27.0
-	gopkg.in/mail.v2 v2.3.1
-)
-
-require (
-	go.uber.org/multierr v1.10.0 // indirect
-	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/containerd/continuity v0.4.3 // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/gotestyourself/gotestyourself v2.2.0+incompatible // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/opencontainers/runc v1.1.13 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	gotest.tools v2.2.0+incompatible // indirect
 )
--- pkg/httpsutil/httpsutil_test.go
+++ pkg/httpsutil/httpsutil_test.go
@@ -1,20 +1,167 @@
 package httpsutil
 
 import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
 	"testing"
+
+	"github.com/joakim-ribier/go-utils/pkg/jsonsutil"
+	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/docker"
 )
 
-// TestCallGetURL calls httpsutil.NewHttpRequest.Call on {GET} URL,
-// checking for a valid return value.
-func TestCallGetURL(t *testing.T) {
-	if r, err := NewHttpRequest("https://github.com/joakim-ribier", ""); r != nil {
-		if r, err := r.Call(); r.StatusCode != 200 {
-			if err != nil {
-				t.Log(err)
-			}
-			t.Errorf(`result: {%d} but expected: {%s}`, r.StatusCode, "200")
-		}
+var exposeHost string = "0.0.0.0:3333"
+
+func TestMain(m *testing.M) {
+	if os.Getenv("ENV_MODE") == "ci" {
+		// on Github action use directly the services container
+		// without dockertest (to have more control on the steps)
+		os.Exit(m.Run())
 	} else {
-		t.Errorf(`result: {%s} but expected: {%s}`, err, "200")
+		// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+		pool, err := dockertest.NewPool("")
+		if err != nil {
+			log.Fatalf("Could not construct pool: %s", err)
+		}
+
+		// uses pool to try to connect to Docker
+		err = pool.Client.Ping()
+		if err != nil {
+			log.Fatalf("Could not connect to Docker: %s", err)
+		}
+
+		resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+			Repository:   "joakimribier/gmocky-v2",
+			Tag:          "latest",
+			Env:          []string{"GMOCKY_PORT=3333"},
+			ExposedPorts: []string{"3333"},
+		}, func(config *docker.HostConfig) {
+			// set AutoRemove to true so that stopped container goes away by itself
+			config.AutoRemove = true
+			config.RestartPolicy = docker.RestartPolicy{Name: "no"}
+		})
+		if err != nil {
+			log.Fatalf("Could not start resource: %s", err)
+		}
+
+		resource.Expire(30) // hard kill the container in 3 minutes (180 Seconds)
+		exposeHost = net.JoinHostPort("0.0.0.0", resource.GetPort("3333/tcp"))
+
+		if err := pool.Retry(func() error {
+			req, err := NewHttpRequest(fmt.Sprintf("http://%s/", exposeHost), "")
+			if err != nil {
+				return err
+			}
+			_, err = req.Timeout("150ms").Call()
+			return err
+		}); err != nil {
+			log.Fatalf("Could not connect to gmocky-v2 server: %s", err)
+		}
+
+		code := m.Run()
+
+		// cannot defer this because os.Exit doesn't care for defer
+		if err := pool.Purge(resource); err != nil {
+			log.Fatalf("Could not purge resource: %s", err)
+		}
+
+		os.Exit(code)
 	}
+}
+
+// TestCall calls httpsutil.NewHttpRequest(string, string).Call(),
+// checking for a valid return value.
+func TestCall(t *testing.T) {
+	uuid := createNewRequest(exposeHost, "", 200, "Hello World")
+
+	req, err := NewHttpRequest(fmt.Sprintf("http://%s/v1/%s", exposeHost, uuid), "")
+	if err != nil {
+		t.Fatalf("Could not build a http request struct: %s", err)
+	}
+
+	resp, err := req.AsJson().Call()
+	if resp.StatusCode != 200 || err != nil || string(resp.Body) != "Hello World" {
+		t.Errorf(`result: {%d} but expected: {%s}`, resp.StatusCode, "200")
+	}
+}
+
+// TestCallWithTimeout calls httpsutil.NewHttpRequest(string, string).Call(),
+// checking for a valid return value.
+func TestCallWithTimeout(t *testing.T) {
+	uuid := createNewRequest(exposeHost, "POST", 200, "Hello World")
+
+	req, err := NewHttpRequest(fmt.Sprintf("http://%s/v1/%s?delay=50ms", exposeHost, uuid), "")
+	if err != nil {
+		t.Fatalf("Could not build a http request struct: %s", err)
+	}
+
+	resp, err := req.Timeout("10ms").AsJson().Call()
+	if !strings.Contains(err.Error(), "(Client.Timeout exceeded while awaiting headers)") {
+		t.Errorf(`result: {%v} but expected: {%v}`, resp, "Client.Timeout exceeded...")
+	}
+}
+
+// TestCallWithNoTimeout calls httpsutil.NewHttpRequest(string, string).Call(),
+// checking for a valid return value.
+func TestCallWithNoTimeout(t *testing.T) {
+	uuid := createNewRequest(exposeHost, "POST", 200, "Hello World")
+
+	req, err := NewHttpRequest(fmt.Sprintf("http://%s/v1/%s?delay=100ms", exposeHost, uuid), "")
+	if err != nil {
+		t.Fatalf("Could not build a http request struct: %s", err)
+	}
+
+	resp, err := req.NoTimeout().AsJson().Call()
+	if err != nil ||
+		req.timeout != 0 ||
+		resp.StatusCode != 200 ||
+		resp.TimeInMillis < 100 || resp.TimeInMillis > 110 {
+		t.Errorf(`result: {%v} but expected: {%s}`, err, "200")
+	}
+}
+
+// TestCallAndTruncateBody calls httpsutil.NewHttpRequest(string, string).Call(),
+// checking for a valid return value.
+func TestCallAndTruncateBody(t *testing.T) {
+	uuid := createNewRequest(exposeHost, "", 200, "Hello World")
+
+	req, err := NewHttpRequest(fmt.Sprintf("http://%s/v1/%s", exposeHost, uuid), "")
+	if err != nil {
+		t.Fatalf("Could not build a http request struct: %s", err)
+	}
+
+	resp, err := req.AsJson().Call()
+
+	if err != nil ||
+		resp.StatusCode != 200 ||
+		resp.TruncateBody(5) != "Hello" {
+		t.Errorf(`result: {%v} but expected: {%s}`, resp, "Hello")
+	}
+}
+
+func createNewRequest(hostAndPort string, method string, status int, body string) string {
+	httpRequest, err := NewHttpRequest(fmt.Sprintf("http://%s/v1/new", hostAndPort), fmt.Sprintf(`{
+    	"status": %d,
+    	"contentType": "text/plain",
+    	"charset": "UTF-8",
+    	"body": "%s"
+	}`, status, body))
+	if err != nil {
+		log.Fatalf("Could not create a http server: %s", err)
+	}
+
+	if method != "" {
+		httpRequest.Method(method)
+	}
+
+	httpResponse, err := httpRequest.AsJson().Call()
+	if err != nil {
+		log.Fatalf("Could not create a http server: %s", err)
+	}
+
+	values, _ := jsonsutil.Unmarshal[map[string]interface{}](httpResponse.Body)
+	return values["uuid"].(string)
 }
